a1a952a58322823eca05daa19f0bb19ecdf544bd
Zhiqi Ye <zhiqiye@Zhiqis-MBP.fios-router.home>
2020-02-27 23:14:43 -0500

    keyme-app master initial commit.

---
+++ keyme-app/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>This is the initialized commit</div>
+        );
+    };
+}
+
+
+export default App;
---
+++ keyme-app/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src') + '/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public') + '/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: require.resolve('babel-loader')
+            },
+            {
+                test:/\.s?css$/,
+                loader: ['style-loader','css-loader', 'sass-loader']
+              }
+        ]
+    }
+}
